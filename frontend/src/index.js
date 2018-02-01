eca6f065c858ff71b9e13d1d78b6461ea42c6362
aditp91 <apothuri@synthesio.com>
2018-02-01 14:21:40 -0500

    config(js): import js from theme and install jquery

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,11 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 import './index.css';
 
-// import '!!style-loader!css-loader! ../vendor/css/style.css';
+import $ from 'jquery'; // instead of - import './plugins/bower_components/jquery/dist/jquery.min.js';
+// import './vendor/bootstrap/dist/js/tether.min.js';
+// import './vendor/bootstrap/dist/js/bootstrap.min.js';
+// import './plugins/bower_components/bootstrap-extension/js/bootstrap-extension.min.js';
+// import './vendor/js/custom.min.js';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
